77bffe3b4cbe8773c3e20187a95e5ae8cba06d28
Mateusz Samsel <m.samsel@cksource.com>
2017-07-17 13:52:07 +0200

    Improve condition in solution. Remove debugger referrence. Add unit test and description to manual

--- plugins/tabletools/plugin.js
+++ plugins/tabletools/plugin.js
@@ -296,7 +296,6 @@
 	}
 
 	function deleteColumns( selection ) {
-		// debugger;
 		var ranges = selection.getRanges(),
 			cells = getSelectedCells( selection ),
 			firstCell = cells[ 0 ],
@@ -314,7 +313,7 @@
 				// #577
 				// Map might contain multiple times this same element, because of existings collspan.
 				// We don't want to overwrite startIndex in such situation and take first one.
-				if ( !startColIndex && map[ i ][ j ] == firstCell.$ ) {
+				if ( startColIndex === undefined && map[ i ][ j ] == firstCell.$ ) {
 					startColIndex = j;
 				}
 				if ( map[ i ][ j ] == lastCell.$ ) {
